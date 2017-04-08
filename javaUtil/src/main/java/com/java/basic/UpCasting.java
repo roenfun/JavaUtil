86b7dddcb8ffa692048464fb37f8d8fd1e9703e7
Chad.long <Chad.long@vipshop.com>
2017-04-08 22:09:13 +0800

    Add：添加向上转型的例子

---
+++ javaUtil/src/main/java/com/java/basic/UpCasting.java
@@ -0,0 +1,28 @@
+package com.java.basic;
+
+/**
+ * Created by chad on 2017/4/8.
+ */
+public class UpCasting {
+    static class Father {
+        public void foo() {
+            System.out.println("One");
+        }
+    }
+    static class Son extends Father {
+        public void foo() {
+            System.out.println("Two");
+        }
+        public void dosth(){
+            System.out.println("Two again");
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        Father father = new Son(); //向上转型，即父类引用指向子类对象，此时子类对象的类型为父类的类型
+        father.foo(); //output: Tow
+        //father.dosth();//编译错误
+        father = (Son)father;
+       // father.dosth();//编译错误
+        ((Son) father).dosth();//编译成功, Tow again
+    }
+}
