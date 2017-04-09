34c763f70ac222ef7c37cb481698a388c50928ab
Chad.long <Chad.long@vipshop.com>
2017-04-09 23:22:20 +0800

    Add:添加多线程的例子

---
+++ javaUtil/src/main/java/com/java/thread/MyThread.java
@@ -0,0 +1,17 @@
+package com.java.thread;
+
+/**
+ * Created by chad on 2017/4/9.
+ */
+public class MyThread extends Thread {
+    private int i = 0;
+
+    //继承Thread类，重写该类的run()方法
+    @Override
+    public void run() {
+        for (i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+
+    }
+}
--- javaUtil/src/test/java/com/java/util/HelloWorldTest.java
+++ javaUtil/src/test/java/com/java/util/HelloWorldTest.java
@@ -1,11 +1,11 @@
 package com.java.util;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.fail;
 
 public class HelloWorldTest {
 
